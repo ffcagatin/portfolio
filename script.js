72d1c1c1e8a17a938b0643d02ced357a7d720077
ffcagatin <ffcagatin@mcm.edu.ph>
2025-11-26 18:55:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const body = document.body;
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
 });
+
+const viewer = document.getElementById("viewer");
+const viewerImg = document.getElementById("viewer-img");
+
+document.querySelector(".achievement-img").onclick = function () {
+    viewerImg.src = this.src;
+    viewer.style.display = "flex";
+};
+
+viewer.onclick = function () {
+    viewer.style.display = "none";
+};
